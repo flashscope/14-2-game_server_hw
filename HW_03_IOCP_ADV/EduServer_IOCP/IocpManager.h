56a8ab72fa1f2b3914bb5052b8176c466f6ce512
1-ChungMoonChul <android_apps@limecolor.com>
2014-07-27 01:40:49 +0900

    iocp adv1 first commit

---
+++ HW_03_IOCP_ADV/EduServer_IOCP/IocpManager.h
@@ -0,0 +1,50 @@
+#pragma once
+
+class ClientSession;
+
+struct OverlappedSendContext;
+struct OverlappedPreRecvContext;
+struct OverlappedRecvContext;
+struct OverlappedDisconnectContext;
+struct OverlappedAcceptContext;
+
+class IocpManager
+{
+public:
+	IocpManager();
+	~IocpManager();
+
+	bool Initialize();
+	void Finalize();
+
+	bool StartIoThreads();
+	void StartAccept();
+
+
+	HANDLE GetComletionPort()	{ return mCompletionPort; }
+	int	GetIoThreadCount()		{ return mIoThreadCount;  }
+
+	SOCKET* GetListenSocket()  { return &mListenSocket;  }
+	LPFN_ACCEPTEX lpfnAcceptEx2 = NULL;
+	LPFN_DISCONNECTEX lpfnDisconnectEx2 = NULL;
+
+private:
+
+	static unsigned int WINAPI IoWorkerThread(LPVOID lpParam);
+
+	static bool PreReceiveCompletion(ClientSession* client, OverlappedPreRecvContext* context, DWORD dwTransferred);
+	static bool ReceiveCompletion(ClientSession* client, OverlappedRecvContext* context, DWORD dwTransferred);
+	static bool SendCompletion(ClientSession* client, OverlappedSendContext* context, DWORD dwTransferred);
+
+private:
+
+	HANDLE	mCompletionPort;
+	int		mIoThreadCount;
+
+	SOCKET	mListenSocket;
+};
+
+extern __declspec(thread) int LIoThreadId;
+extern IocpManager* GIocpManager;
+
+
